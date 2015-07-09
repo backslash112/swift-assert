bf199ee2386fded8ceea9c134516c5fe21ad3089
YangCun <yangcun@live.com>
2015-07-09 11:33:12 +0800

    Create the project.

---
+++ swift-assert/swift-assert/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-assert
+//
+//  Created by Carl.Yang on 7/9/15.
+//  Copyright (c) 2015 Carl.Yang. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
